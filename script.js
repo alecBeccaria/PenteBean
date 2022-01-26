535864c7caa4d4aa81e1b43184b4cfc30fbed869
nathanielmalmrose <79717751+nathanielmalmrose@users.noreply.github.com>
2022-01-25 19:02:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("HI");
+
+//  Origin Piece is placed
+
+//  Check if Piece occupies space next to it
+    //  If piece exists, Check color of piece next to Origin Piece
+        // If same color, count number of pieces in row
+            //  If 3 in row, announce tria
+            //  If 4 in row, announce tessera
+            //  If 5 in row, announce win
+        //  If opponent color
+            //  Check for 2 in row
+                //  If 3rd in row is same color, capture
