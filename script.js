aee9456c40e73b08f91db6f38054339eb896333f
docsteampunk <larmore.daniel@gmail.com>
2022-02-01 19:01:41 -0700

    Changed data

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var imageSrc = "placeholder.jpg";
+var imageSrc1 = "Black_sm.png";
+var imageSrc2 = "White_sm.png"
 var clickedRow;
 var clickedColumn;
+var isFirsPlayerTurn = true;
 
 
 
@@ -11,7 +13,7 @@ for (let i = 0; i < 19; i++) {
     gameBoard.push(row);
 }
 console.log(gameBoard);
-gameBoard[2][3] = "test";
+//gameBoard[2][3] = "test";
 console.log(gameBoard);
 
 var tableDiv = document.getElementById("tableId");
@@ -28,13 +30,21 @@ for(let a = 0; a < gameBoard.length; a++){
     var tr = document.createElement('TR');
     for(let b = 0; b < gameBoard[a].length; b++){
         var td = document.createElement('TD');
-        td.appendChild(document.createTextNode(gameBoard[a][b]));
+        if (gameBoard[a][b] != 1) {
+            td.appendChild(document.createTextNode(gameBoard[a][b]));
+        }
+        else {
+            td.appendChild(document.createTextNode(" "));
+        }
         //Split on forwardSlashes for getting the ids
         td.id = "tableCell/" + a + "/" + b;
         td.className = "tableCells";
 
         td.onclick = function() {
             getVal(this)
+        }
+        if ((a == 6 && b == 6) || (a == 6 && b == 12) || (a == 12 && b == 12) || (a == 12 && b == 6) || (a == 9 && b == 9)) {
+            td.classList.add("starting");
         }
         tr.appendChild(td);
     }
@@ -50,10 +60,15 @@ console.log(allCells)
 function getVal(cell){
     cell.innerHTML = "";
     var img = document.createElement("IMG")
-    img.src = imageSrc;
+    if (isFirsPlayerTurn) {
+        img.src = imageSrc1;
+    }
+    else {
+        img.src = imageSrc2;
+    }
+    isFirsPlayerTurn = !isFirsPlayerTurn;
     cell.appendChild(img)
 
-    alert(cell.innerHTML);
     let words = cell.id.split("/");
     clickedRow = words[1];
     clickedColumn = words[2];
